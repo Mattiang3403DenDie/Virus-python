6c62fad6d801af291e0e55735829088064b7cd42
Mattiang3403DenDie <serena1977.08.17@gmail.com>
2025-12-02 17:27:08 +0100

    Virus.py
    Virus made by python

---
+++ Virus.py
@@ -0,0 +1,7 @@
+from colorama import Fore
+import math
+import random
+while True:
+    print(math.sqrt(25)-math.pow(random.randrange(55),12+random.randrange(10)))
+    print(Fore.RED+"YOU HAVE",Fore.GREEN+"BEEN",Fore.BLUE+"HACKED",Fore.YELLOW+"BY MATTIAS TEAM")
+    print(Fore.BLUE+"NO",Fore.GREEN+" C",Fore.YELLOW+"HA",Fore.RED+"N!",Fore.CYAN+"CE!")
